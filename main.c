4386cd1cf490cfa79071f608bb7cbe7dd69eb7e5
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-22 17:12:03 -0600

    Placed a GI test call.

--- main.c
+++ main.c
@@ -24,12 +24,7 @@
 */
 
 #include <gtk/gtk.h>
-
-//todo: remove unnecessary headers.
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <girepository.h>
 
 #define JIM_EMBEDDED
 #include <jim.h>
@@ -65,12 +60,57 @@ static void app_open (GApplication *application,
         }
         g_free(path);
     }
+
+    // test call thru GI
+    GIRepository *repository;
+    GError *error = NULL;
+    GIBaseInfo *base_info;
+    GIArgument in_args[5];
+    GIArgument retval;
+
+    repository = g_irepository_get_default ();
+    g_irepository_require (repository, "GLib", "2.0", 0, &error);
+    if (error)
+    {
+      g_error ("ERROR: %s\n", error->message);
+      return;
+    }
+
+    base_info = g_irepository_find_by_name (repository, "GLib", "assertion_message");
+    if (!base_info)
+    {
+      g_error ("ERROR: %s\n", "Could not find GLib.warn_message");
+      return;
+    }
+
+    in_args[0].v_pointer = (gpointer)"domain";
+    in_args[1].v_pointer = (gpointer)"glib-print.c";
+    in_args[2].v_int = 42;
+    in_args[3].v_pointer = (gpointer)"main";
+    in_args[4].v_pointer = (gpointer)"hello world";
+
+    if (!g_function_info_invoke ((GIFunctionInfo *) base_info,
+                               (const GIArgument *) &in_args,
+                               5,
+                               NULL,
+                               0,
+                               &retval,
+                               &error))
+    {
+      g_error ("ERROR: %s\n", error->message);
+      return;
+    }
+
+    g_base_info_unref (base_info);
 }
 
 int main (int argc, char **argv) {
     // prepare Jim interp.
     itp = Jim_CreateInterp();
-    assert(itp != NULL && "couldn't create interpreter");
+    if (itp == NULL) {
+        fprintf(stderr, "%s", "couldn't create interpreter\n");
+        return 1;
+    }
     Jim_RegisterCoreCommands(itp);
     Jim_InitStaticExtensions(itp);
     //todo: package provide gizmo, so dlr pkg can detect that, and enable declaring gnome calls.
