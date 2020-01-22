01c3ffb834df3119c0fbea0d5f2bdf22fe51276b
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-22 11:51:35 -0600

    Accept command line name of script to run.

--- main.c
+++ main.c
@@ -2,29 +2,30 @@
   gizmo
   Copyright 2020 Mark Hubbard, a.k.a. "TheMarkitecht"
   http://www.TheMarkitecht.com
-   
+
   Project home:  http://github.com/TheMarkitecht/gizmo
   gizmo is a GNOME / GTK+ 3 windowing shell for Jim Tcl (http://jim.tcl.tk/)
   gizmo helps you quickly develop modern cross-platform GUI apps in Tcl.
-   
+
   This file is part of gizmo.
-   
+
   gizmo is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-   
+
   gizmo is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
-   
+
   You should have received a copy of the GNU Lesser General Public License
   along with gizmo.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <gtk/gtk.h>
 
+//todo: remove unnecessary headers.
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,43 +36,53 @@
 
 // links to libgtk-3.so.0.2404.1
 
-Jim_Interp *interp;
+Jim_Interp *itp;
 
-static void activate (GtkApplication* app,  gpointer user_data)
-{
+//todo: bind g_free()
+
+static void app_activate (GtkApplication* app,  gpointer user_data) {
+    //todo: move to script.
     GtkWidget *window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Window");
     gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
     gtk_widget_show_all (window);
-    
-	/* Run a script. */
-	if (Jim_Eval(interp, "puts {Jim ALIVE!!!}") == JIM_ERR) {
-		Jim_MakeErrorMessage(interp);
-		fprintf(stderr, "%s\n", Jim_GetString(Jim_GetResult(interp), NULL));
-	}    
 }
 
-int main (int argc, char **argv)
-{
-	/* Create an interpreter. */
-	interp = Jim_CreateInterp();
-	assert(interp != NULL && "couldn't create interpreter");
+static void app_open (GApplication *application,
+               gpointer      files,
+               gint          n_files,
+               gchar        *hint,
+               gpointer      user_data) {
 
-	/* We register base commands, so that we actually implement Tcl. */
-	Jim_RegisterCoreCommands(interp);
+    // run scripts named on command line.
+    GFile** fArray = (GFile**)files;
+    for (int i = 0; i < n_files; i++) {
+        char* path = g_file_get_path(G_FILE(fArray[i]));
+        if (Jim_EvalFileGlobal(itp, path) != JIM_OK) {
+            Jim_MakeErrorMessage(itp);
+            fprintf(stderr, "%s\n", Jim_GetString(Jim_GetResult(itp), NULL));
+            gtk_main_quit(); //todo: use correct exit function, and set exit status
+        }
+        g_free(path);
+    }
+}
 
-	/* And initialise any static extensions */
-	Jim_InitStaticExtensions(interp);
+int main (int argc, char **argv) {
+    // prepare Jim interp.
+    itp = Jim_CreateInterp();
+    assert(itp != NULL && "couldn't create interpreter");
+    Jim_RegisterCoreCommands(itp);
+    Jim_InitStaticExtensions(itp);
 
+    // prepare GNOME.
     GtkApplication *app;
-    int status;
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_HANDLES_OPEN);
+    g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
+    g_signal_connect (app, "open", G_CALLBACK (app_open), NULL);
+    int status = g_application_run (G_APPLICATION (app), argc, argv);
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
+    // clean up.
     g_object_unref (app);
-
-	Jim_FreeInterp(interp);
-
+    Jim_FreeInterp(itp);
     return status;
 }
